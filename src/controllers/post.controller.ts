49398adc0064c470c78efc621a62da25dd0c276e
Projoi <assamipro80@gmail.com>
2024-09-11 17:30:14 +0700

    new api get all match posts

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import Post from "../models/Post";
 import { encrypt, decrypt } from "../config/crypto";
 import Skills from "../models/Skills";
+import jwt from "jsonwebtoken"
 import Locations from "../models/Locations";
+import User from "../models/User";
+import { Op } from "sequelize";
 
 
 export const getAllPost = async (req: Request, res: Response) => {
@@ -55,6 +58,79 @@ export const getAllPost = async (req: Request, res: Response) => {
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
+};
+
+export const getAllMatchPost = async (req: Request, res: Response) => {
+  const search = req.query.search || ""
+  const post_date = req.query.post_date || "DESC"
+  let skills:any = req.query.skills || "[]"
+  let type:any = req.query.type || "[]"
+  let locations:any = req.query.locations || "[]"
+
+  let db_page = req.query.page || 1
+  let db_limit = req.query.limit || 20
+  locations = JSON.parse(locations)
+  type = JSON.parse(type)
+  skills = JSON.parse(skills)
+
+  if (search !== "" || locations.length > 0 || skills.length > 0 || type.length > 0) {
+    db_limit = 999;
+  }
+
+  try {
+    const userAuth = req.headers.authorization
+    try {
+        jwt.verify(userAuth, process.env.ACCESS_TOKEN_SECRET, async function (err, user:any){
+            // if(err) return res.status(200).json({message: "Unauthorized, refresh token invalid"})
+            if(err) return res.status(400).json(err.message)
+            const USER = await User.findByPk(user.id)
+            if(!USER) return res.status(404).json({message: "user not found"})
+
+            const SKILLS = await USER.getSkills()
+            if (SKILLS.length === 0) return res.status(200).json([]);
+            const skillNames = SKILLS.map(skill => skill.dataValues.skill);
+
+            const POST = await Post.findAll({
+              attributes: { exclude: ["updatedAt"] },
+              limit: +db_limit,
+              offset: (+db_page - 1) * +db_limit,
+              order: [["post_date", post_date.toString()]],
+              include: [
+                {
+                    model: Skills,
+                    as: "skills",
+                }
+            ]
+            });
+
+            let filtered_data = POST.filter(post => {
+              let post_json = post.toJSON();
+              const matchesSearch = post_json.title.toLowerCase().includes(search.toString().toLowerCase()) || post_json.company.toLowerCase().includes(search.toString().toLowerCase());
+              const matchesSkills = post_json.skills.some(skill => skillNames.includes(skill.skill));
+              return matchesSearch && matchesSkills;
+            })
+            
+            
+      
+            const total_entries = filtered_data.length;
+            const total_pages = Math.ceil(total_entries / +db_limit);
+      
+            let encryptedData = encrypt({
+              limit: db_limit,
+              page: db_page,
+              total_page: total_pages,
+              datas: filtered_data
+            }, process.env.AES_KEYS)
+                
+            return res.status(200).json(encryptedData)
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(200).json({message: error.message})
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 export const getPostById = async (req: Request, res: Response) => {
--- src/router/post.router.ts
+++ src/router/post.router.ts
@@ -1,9 +1,10 @@
-import { getAllPost, updatePosts, DeletePost, addPost, getAllPostCount, getPostCount, getPostById } from "../controllers/post.controller";
+import { getAllPost, updatePosts, DeletePost, addPost, getAllPostCount, getPostCount, getPostById, getAllMatchPost } from "../controllers/post.controller";
 import express from "express";
     
 const postRouter = express.Router();
 
 postRouter.get("/", getAllPost)
+postRouter.get("/match", getAllMatchPost)
 postRouter.get("/count", getPostCount)
 postRouter.get("/count-all", getAllPostCount)
 postRouter.get("/:id", getPostById)
